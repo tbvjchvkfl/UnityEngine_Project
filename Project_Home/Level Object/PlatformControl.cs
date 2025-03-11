9d76bfb9186402af64cbaa32dd83cc50883d911f
tbvjchvkfl / Developer <137769043+tbvjchvkfl@users.noreply.github.com>
2025-03-11 22:32:10 +0900

    Add files via upload

--- Project_Home/Level Object/PlatformControl.cs
+++ Project_Home/Level Object/PlatformControl.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlatformControl : MonoBehaviour
@@ -12,8 +14,14 @@ public class PlatformControl : MonoBehaviour
     [Header("Object")]
     public GameObject ControlledPlatform;
 
+    [Header("Data Construct")]
+    public List<GameObject> TargetObjs;
+
     [Header("Value")]
     public float RotateSpeed;
+    public float ObjectLoc;
+    public float ReturnSpeed;
+
 
     [HideInInspector] public bool bIsInteracting;
 
@@ -23,8 +31,11 @@ public class PlatformControl : MonoBehaviour
     // ====================================
     Transform PlatformWorldTransform;
     Transform TargetCharacterTransform;
+    Vector3 InitPos;
     PlayerInput TargetCharacterInput;
     bool bIsRotationStart;
+    bool PullMax;
+    
 
     void Awake()
     {
@@ -37,16 +48,13 @@ public class PlatformControl : MonoBehaviour
             TargetCharacterTransform = TargetCharacter.gameObject.GetComponent<Transform>();
             TargetCharacterInput = TargetCharacter.GetComponent<PlayerInput>();
         }
+        InitPos = transform.position;
     }
 
     void Update()
     {
         SetPlatformRotation();
-        if (bIsInteracting)
-        {
-            //transform.Translate(TargetCharacterTransform.position);
-            transform.position = TargetCharacterTransform.position;//new Vector3(TargetCharacterInput.GetCharacterCapsule().bounds.size.x / 2, 0.0f, 0.0f);
-        }
+        MovePullTrigger();
     }
 
     void SetPlatformRotation()
@@ -81,5 +89,39 @@ public class PlatformControl : MonoBehaviour
         {
             bIsRotationStart = false;
         }
+    }
+
+    public bool GetPullMax()
+    {
+        return PullMax;
+    }
+
+    void MovePullTrigger()
+    {
+        CaculatePercent();
+        if (bIsInteracting)
+        {
+            transform.position = new Vector3(TargetCharacterTransform.position.x + ObjectLoc, transform.position.y, transform.position.z);
+
+            if (transform.position.x <= 0.2f)
+            {
+                transform.position = new Vector3(0.2f, transform.position.y, transform.position.z);
+                PullMax = true;
+            }
+            else
+            {
+                PullMax = false;
+            }
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, InitPos, ReturnSpeed * Time.deltaTime);
+        }
+    }
+
+    void CaculatePercent()
+    {
+        float MaxValue = Mathf.Abs(transform.position.x * 100.0f);
+        Debug.Log(MaxValue);
     }
 }
