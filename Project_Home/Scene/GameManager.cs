a58260f80c6793c4e311424c83625883907b21b4
tbvjchvkfl / Developer <137769043+tbvjchvkfl@users.noreply.github.com>
2025-04-19 00:19:03 +0900

    게임 매니저 & 사운드 매니저 코드 최종 작업완료

--- Project_Home/Scene/GameManager.cs
+++ Project_Home/Scene/GameManager.cs
@@ -1,9 +1,27 @@
-using NUnit.Framework.Internal;
+using System.IO;
 using UnityEngine;
+using UnityEngine.LowLevel;
+using UnityEngine.SceneManagement;
+
+[System.Serializable]
+public class SaveData
+{
+    public bool FirstHeartObj;
+    public bool SecondHeartObj;
+    public bool ThirdHeartObj;
+
+    public Vector2 PlayerLoc;
+    public int PlayerHP;
+}
 
 public class GameManager : MonoBehaviour
 {
     GameObject PlayerCharacter;
+    GameObject EnemyCharacter;
+
+    GameObject FObj;
+    GameObject SObj;
+    GameObject TObj;
 
     public static GameManager Instance{ get; private set; }
 
@@ -15,10 +33,23 @@ public class GameManager : MonoBehaviour
     {
         get { return PlayerCharacter.GetComponent<PlayerInput>(); }
     }
-    public int PlayerHP {  get; private set; }
+    
+    public bool bIsGameOver { get; private set; }
+    public bool bIsBossBattle {  get; private set; }
+
+    // SaveData
+    public Vector2 PlayerLocation { get; private set; }
+    public int PlayerHP { get; private set; }
+    public int StageNumber { get; private set; }
+    public bool bIsFHeartObject {  get; set; }
+    public bool bIsSHeartObject { get; set; }
+    public bool bIsTHeartObject { get; set; }
+    
+    public string SavePath {  get; private set; }
 
     void Awake()
     {
+        SavePath = Path.Combine(Application.persistentDataPath, "SaveData.json");
         if (!Instance)
         {
             Instance = this;
@@ -31,36 +62,117 @@ public class GameManager : MonoBehaviour
         DontDestroyOnLoad(this.gameObject);
     }
 
+    void Start()
+    {
+        LoadGame();
+        DestroyHeartObj();
+    }
+
     void Update()
     {
+        InitEssentialData();
     }
 
     void InitEssentialData()
     {
-        if (!PlayerCharacter)
+        if (SceneManager.GetActiveScene().buildIndex == 1)
         {
-            if (PlayerCharacter = GameObject.FindGameObjectWithTag("Player"))
+            if (!PlayerCharacter)
             {
-                Debug.Log("Find PC");
+                PlayerCharacter = GameObject.FindGameObjectWithTag("Player");
+                FObj = GameObject.FindGameObjectWithTag("FHObj");
+                SObj = GameObject.FindGameObjectWithTag("SHObj");
+                TObj = GameObject.FindGameObjectWithTag("THObj");
+                StageNumber = 1;
+                bIsGameOver = false;
+                LoadGame();
+                DestroyHeartObj();
+            }
+        }
+        if (SceneManager.GetActiveScene().buildIndex == 2)
+        {
+            if (!PlayerCharacter)
+            {
+                PlayerCharacter = GameObject.FindGameObjectWithTag("Player");
+                StageNumber = 2;
+                bIsGameOver = false;
+            }
+            if (!EnemyCharacter)
+            {
+                EnemyCharacter = GameObject.FindGameObjectWithTag("Enemy");
+                bIsBossBattle = true;
             }
         }
     }
 
     public void ApplyPlayerHP()
     {
+        bIsGameOver = false;
         PlayerHP++;
-        PCInfo.GetGameManagerData();
         HUD.Instance.PlayerHealthBar.ShowPlayerUI();
     }
 
     public void SetPlayerHP(int Value)
     {
         PlayerHP -= Value;
+        if (PlayerHP <= 0)
+        {
+            bIsGameOver = true;
+        }
         HUD.Instance.PlayerHealthBar.ShowPlayerUI();
     }
 
-    public int LoadPlayerHP()
+    void DestroyHeartObj()
     {
-        return PlayerHP;
+        if (bIsFHeartObject && FObj)
+        {
+            Destroy(FObj.gameObject);
+        }
+        if (bIsSHeartObject && SObj)
+        {
+            Destroy(SObj.gameObject);
+        }
+        if (bIsTHeartObject && TObj)
+        {
+            Destroy(TObj.gameObject);
+        }
+    }
+
+    public void SaveGame()
+    {
+        SaveData Data = new SaveData
+        {
+            PlayerLoc = PlayerCharacter.transform.position,
+            PlayerHP = PlayerHP,
+            FirstHeartObj = bIsFHeartObject,
+            SecondHeartObj = bIsSHeartObject,
+            ThirdHeartObj = bIsTHeartObject
+        };
+
+        string JSONSave = JsonUtility.ToJson(Data, true);
+        File.WriteAllText(SavePath, JSONSave);
+    }
+
+    public void LoadGame()
+    {
+        if (File.Exists(SavePath))
+        {
+            string JSONSave = File.ReadAllText(SavePath);
+            SaveData Data = JsonUtility.FromJson<SaveData>(JSONSave);
+
+            PlayerLocation = Data.PlayerLoc;
+            PlayerHP = Data.PlayerHP;
+            bIsFHeartObject = Data.FirstHeartObj;
+            bIsSHeartObject = Data.SecondHeartObj;
+            bIsTHeartObject = Data.ThirdHeartObj;
+            PCInfo.GetGameManagerData();
+        }
+        else
+        {
+            PlayerHP = 0;
+            bIsFHeartObject = false;
+            bIsSHeartObject = false;
+            bIsTHeartObject = false;
+        }
     }
 }
