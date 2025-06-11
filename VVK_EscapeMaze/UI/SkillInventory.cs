33934fee451673f4bc81524fecc3b8d2aadaf8d5
tbvjchvkfl / Developer <137769043+tbvjchvkfl@users.noreply.github.com>
2025-06-11 21:13:42 +0900

    Skill UI 작업 시작

---
+++ VVK_EscapeMaze/UI/SkillInventory.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class SkillInventory : MonoBehaviour
+{
+    public GameObject PanelSlotObj;
+
+    public void InitSkillInventory(PlayerInventory playerInventory)
+    {
+
+    }
+
+
+}
