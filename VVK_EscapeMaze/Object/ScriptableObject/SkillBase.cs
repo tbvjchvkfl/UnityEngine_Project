eb5427f8f8191b4a66f1dee4f8acefac25bb7681
tbvjchvkfl / Developer <137769043+tbvjchvkfl@users.noreply.github.com>
2025-06-16 21:15:39 +0900

    스킬 작업 완료

---
+++ VVK_EscapeMaze/Object/ScriptableObject/SkillBase.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SkillBase", menuName = "Scriptable Objects/SkillBase")]
+public class SkillBase : ScriptableObject
+{
+    public string SkillName = "New Skill";
+    public string SkillDescription = "Skill Description";
+
+    public int SkillID = 0;
+    public int NeedTechnicalPoint = 1;
+
+    public float IncreaseAttackRate = 1.0f;
+    public float IncreaseAimRate = 1.0f;
+    public float IncreaseArmorRate = 1.0f;
+
+    public bool bIsActivity = false;
+
+    public Sprite SkillIcon = null;
+
+    public SkillBase CopyData()
+    {
+        var Copy = CreateInstance<SkillBase>();
+        Copy.SkillName = SkillName;
+        Copy.SkillDescription = SkillDescription;
+        Copy.SkillID = SkillID;
+        Copy.NeedTechnicalPoint = NeedTechnicalPoint;
+        Copy.IncreaseAttackRate = IncreaseAttackRate;
+        Copy.IncreaseAimRate = IncreaseAimRate;
+        Copy.IncreaseArmorRate = IncreaseArmorRate;
+        Copy.bIsActivity = bIsActivity;
+        Copy.SkillIcon = SkillIcon;
+
+        return Copy;
+    }
+}
