01cf39bf26d45386701284bb89daf2aa2cdad030
tbvjchvkfl / Developer <137769043+tbvjchvkfl@users.noreply.github.com>
2025-06-26 21:04:56 +0900

    문 구조 수정

--- VVK_EscapeMaze/Object/LevelObject/LO_Door.cs
+++ VVK_EscapeMaze/Object/LevelObject/LO_Door.cs
@@ -3,28 +3,64 @@ using UnityEngine;
 
 public class LO_Door : MonoBehaviour
 {
-    public GameObject DoorObj;
+    public GameObject LeftDoor;
+    public GameObject RightDoor;
 
-    void Awake()
+
+    Vector3 LeftDoorInitPos;
+    Vector3 RightDoorInitPos;
+    Coroutine currentCoroutine;
+
+    void Start()
     {
+        LeftDoorInitPos = LeftDoor.transform.localPosition;
+        RightDoorInitPos = RightDoor.transform.localPosition;
     }
 
-    public void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            StartCoroutine(OnOpenDoor());
-            
+            if (currentCoroutine == null)
+            {
+                currentCoroutine = StartCoroutine(OnOpenDoor());
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+       if (other.gameObject.tag == "Player")
+        {
+            if (currentCoroutine != null)
+            {
+                StopCoroutine(currentCoroutine);
+                currentCoroutine = null;
+            }
+            currentCoroutine = StartCoroutine(OnCloseDoor());
         }
     }
 
     IEnumerator OnOpenDoor()
     {
-        while (DoorObj.transform.position.y < 10.0f)
+        while (LeftDoor.transform.localPosition.x < 2.5f && RightDoor.transform.localPosition.x > -2.5f)
         {
-            DoorObj.transform.position = Vector3.up * 2.0f * Time.deltaTime;
+            LeftDoor.transform.localPosition += new Vector3(2.0f * Time.deltaTime, 0.0f, 0.0f);
+            RightDoor.transform.localPosition -= new Vector3(2.0f * Time.deltaTime, 0.0f, 0.0f);
+            yield return null;
         }
-        
-        yield return null;
+        currentCoroutine = null;
     }
+
+    IEnumerator OnCloseDoor()
+    {
+        while (LeftDoor.transform.localPosition.x > LeftDoorInitPos.x && RightDoor.transform.localPosition.x < RightDoorInitPos.x)
+        {
+            LeftDoor.transform.localPosition -= new Vector3(2.0f * Time.deltaTime, 0.0f, 0.0f);
+            RightDoor.transform.localPosition += new Vector3(2.0f * Time.deltaTime, 0.0f, 0.0f);
+            yield return null;
+        }
+        currentCoroutine = null;
+    }
+
 }
