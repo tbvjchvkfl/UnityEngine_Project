dcd93958c271fc82b4957b1a475c6dacdfd9e102
tbvjchvkfl / Developer <137769043+tbvjchvkfl@users.noreply.github.com>
2025-07-10 21:30:56 +0900

    EnemyBase 추ㅜ가

--- VVK_EscapeMaze/Data/Enemy/EnemyBase.cs
+++ VVK_EscapeMaze/Data/Enemy/EnemyBase.cs
@@ -1,1 +1,30 @@
+using UnityEngine;
 
+[System.Serializable]
+public class StatusData
+{
+    public float MaxHP;
+    public float AttackRate;
+    public float DefenseRate;
+    public float DodgeRate;
+}
+
+[System.Serializable]
+public class EnemyPrefabData
+{
+    public GameObject EnemyPrefab;
+}
+
+[System.Serializable]
+public class DropItemData
+{
+    public GameObject ItemPrefab;
+}
+
+[CreateAssetMenu(fileName = "EnemyBase", menuName = "Scriptable Objects/EnemyBase")]
+public class EnemyBase : ScriptableObject
+{
+    public StatusData statusData = new StatusData();
+    public EnemyPrefabData enemyPrefabData = new EnemyPrefabData();
+    public DropItemData dropItemData = new DropItemData();
+}
